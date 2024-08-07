d93cd8458adb928821f2e24779b937a05fc1c465
Alen James <alenjames899@gmail.com>
2024-08-07 13:06:52 +0530

    Update project with new changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,4 +9,15 @@ export default defineConfig({
       "/uploads/": "http://localhost:5000",
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 });
